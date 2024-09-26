c2df9751d14ac1fd9d71629572b1e27223ef4ef6
Alexandre Rafaschi <alexandre.rafasky@gmail.com>
2024-09-26 00:11:01 -0300

    aula 4

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,18 +1,21 @@
 alert('Boas vindas ao jogo do número secreto');
-let numeroSecreto = 15;
+let numeroSecreto = parseInt(Math.random() *1000 + 1);
 let chute
 let tentativas = 0
 
 while (chute != numeroSecreto) {
-    chute = prompt('Escolha um número entre 1 e 30. Tentativas: ' + tentativas);
+    chute = prompt('Escolha um número entre 1 e 1000. Tentativas: ' + tentativas);
     tentativas++
+    let quantasTentativas = tentativas > 1 ? 'tentativas' : 'tentativa';
 
     if (chute == numeroSecreto) {
-        alert('Certo, o número é ' + numeroSecreto + ' e você usou ' + tentativas + ' tentativas');
-    } else { 
+        alert('Certo, o número é ' + numeroSecreto + ' e você usou ' + tentativas + ' ' + quantasTentativas);
+    } 
+    else { 
         if (chute < numeroSecreto) {
             alert('O número secreto é maior que ' + chute)
-        } else {
+        } 
+        else {
             alert('O número secreto é menor que ' + chute)
         }
     }
